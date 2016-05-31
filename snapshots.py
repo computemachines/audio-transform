1f27035e30ef41eb10c84d19e34aa8d968422329
Tyler Parker <tparker@umass.edu>
2016-05-31 08:17:07 -0400

    Call wake() after startActivity

--- snapshots.py
+++ snapshots.py
@@ -8,9 +8,12 @@ package = "com.computemachines.audiotransform"
 finderActivity = ".FinderActivity"
 
 for n in range(25):
-    print "taking snapshot at "+str("(time.localtime()")
+    print "taking snapshot at "+str(time.localtime())
     if n == 5:
         device.startActivity(component=package+"/"+finderActivity)
+    if n == 10:
+        # tap in center of device
+        device.wake()
     result = device.takeSnapshot()
     result.writeToFile("activity_finder_%02d.png" % (n), "png")
     time.sleep(60)
