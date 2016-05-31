763af4516c7568ed18204b9a03f8d9c1123ff434
Tyler Parker <tparker@umass.edu>
2016-05-31 02:54:33 -0400

    Attempt to start activity

--- snapshots.py
+++ snapshots.py
@@ -7,9 +7,10 @@ device = MonkeyRunner.waitForConnection()
 package = "com.computemachines.audiotransform"
 finderActivity = ".FinderActivity"
 
-for n in range(5):
+for n in range(25):
     print "taking snapshot at "+str("(time.localtime()")
-    # device.startActivity(component=package+"/"+finderActivity)
+    if n == 5:
+        device.startActivity(component=package+"/"+finderActivity)
     result = device.takeSnapshot()
     result.writeToFile("activity_finder_%02d.png" % (n), "png")
     time.sleep(60)
