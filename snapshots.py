6ce08457700bb428cabbc8c4c2a2a1d5cb150d17
Tyler Parker <tparker@umass.edu>
2016-05-30 23:34:57 -0400

    Debugging CircleCI android emulator

--- snapshots.py
+++ snapshots.py
@@ -1,11 +1,15 @@
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+import time
+
 
 device = MonkeyRunner.waitForConnection()
 
 package = "com.computemachines.audiotransform"
 finderActivity = ".FinderActivity"
 
-
-device.startActivity(component=package+"/"+finderActivity)
-result = device.takeSnapshot()
-result.writeToFile("activity_finder.png", "png")
+for _ in range(5):
+    print "taking snapshot at {}".format(time.localtime())
+    # device.startActivity(component=package+"/"+finderActivity)
+    result = device.takeSnapshot()
+    result.writeToFile("activity_finder.png", "png")
+    time.sleep(60)
