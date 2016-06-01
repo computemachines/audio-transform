9e612da73b70c2477ef0b24fa9161ef63c247c25
Tyler Parker <tparker@umass.edu>
2016-05-31 23:11:40 -0400

    Use ImageMagick montage to combine screenshots

--- snapshots.py
+++ snapshots.py
@@ -7,13 +7,17 @@ device = MonkeyRunner.waitForConnection()
 package = "com.computemachines.audiotransform"
 finderActivity = ".FinderActivity"
 
+result = device.takeSnapshot()
+result.writeToFile("system_initial.png" % (n), "png")
+
 for n in range(25):
-    print "taking snapshot at "+str(time.localtime())
+    print "current time: "+str(time.localtime())
     if n == 5:
         device.startActivity(component=package+"/"+finderActivity)
     if n == 10:
         # tap in center region
         device.touch(300, 500, MonkeyDevice.DOWN_AND_UP)
-    result = device.takeSnapshot()
-    result.writeToFile("activity_finder_%02d.png" % (n), "png")
+    if n == 15:
+        result = device.takeSnapshot()
+        result.writeToFile("activity_finder.png" % (n), "png")
     time.sleep(60)
