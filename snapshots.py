f8b9de33d7dcfa8e207b07411860270cfee5e886
Tyler Parker <tparker@umass.edu>
2016-06-01 09:33:42 -0400

    Add logging. Fix image dependency early deletion.

--- snapshots.py
+++ snapshots.py
@@ -13,11 +13,14 @@ result.writeToFile("system_initial.png", "png")
 for n in range(16):
     print "current time: "+str(time.localtime())
     if n == 5:
+        print "starting activity"
         device.startActivity(component=package+"/"+finderActivity)
     if n == 10:
+        print "touching center"
         # tap in center region
         device.touch(300, 500, MonkeyDevice.DOWN_AND_UP)
     if n == 15:
+        print "taking snapshot"
         result = device.takeSnapshot()
         result.writeToFile("activity_finder.png", "png")
     time.sleep(60)
